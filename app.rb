b7f306f122bc04125aab359d034ce752171eabcf
Donald Brooks Arum <brooksd079@gmail.com>
2022-08-12 03:43:12 +0300

    commit tree 33db5352d4d943c0c9f41e311fa5f26e05f63074

---
+++ app.rb
@@ -0,0 +1,14 @@
+puts "Hello World!"
+# puts "Hello World!"
+# puts "Hello World!"
+
+print "Pass this test, please."
+ # print "Hello World!"
+ # print "Hello World!"
+
+# puts [1, 2, 3] #ruby forces the array into a string using the to_s method
+p [1, 2, 3] # prints oyt the entire array properly by calling the inspect methodt
+
+# #pretty printing will call on the pretty inspect methord to properly format this
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
